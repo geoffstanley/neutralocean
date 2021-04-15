0f3020ce9b0c92ce9f9612dfec4341de716dd9eb
Geoff Stanley <geoffstanley@gmail.com>
2021-04-16 09:04:20 +1000

    updated tests; 17 pass, but 3 fail due to typing errors??

--- tests/test_interp.py
+++ tests/test_interp.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from neutral_surfaces._interp import linear_coefficients, linear_eval2
+from neutral_surfaces.interp_ppc import linear_coefficients, val2
 
 # Monotonic but non-uniform pressure grid.
 npg = 50
@@ -12,8 +12,8 @@ S *= 0.1 * (pgrid / pgrid[-1]) ** 2
 T = np.ones((ncasts, npg)) + np.arange(ncasts)[:, np.newaxis] * 0.01
 T *= 10 * (pgrid / pgrid[-1]) ** 3
 
-coefs_T = linear_coefficients(pgrid, T)
-coefs_S = linear_coefficients(pgrid, S)
+Tppc = linear_coefficients(pgrid, T)
+Sppc = linear_coefficients(pgrid, S)
 
 
 def test_linear():
@@ -24,12 +24,18 @@ def test_linear():
     expected_S = np.empty_like(expected_T)
     result_S = np.empty_like(expected_T)
     result_T = np.empty_like(expected_T)
+    
+    d_vert = S.ndim -1 # index to vertical dimension
+    
+    assert Sppc.shape[d_vert] == S.shape[d_vert] - 1
+    assert Tppc.shape[d_vert] == T.shape[d_vert] - 1
+    
     for i in range(ncasts):
         expected_T[i] = np.interp(targets, pgrid, T[i])
         expected_S[i] = np.interp(targets, pgrid, S[i])
         for j in range(ntargets):
-            result_S[i, j], result_T[i, j] = linear_eval2(
-                targets[j], pgrid, S[i], coefs_S[i], T[i], coefs_T[i]
+            result_S[i, j], result_T[i, j] = val2(
+                pgrid, S[i], Sppc[i], T[i], Tppc[i], targets[j]
             )
     assert np.allclose(result_S, expected_S)
     assert np.allclose(result_T, expected_T)
