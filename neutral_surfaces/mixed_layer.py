53e6731482d83aa36613ced327fa52b88e53c6a6
Geoff Stanley <geoffstanley@gmail.com>
2022-01-06 11:34:22 +1100

    bottle_num 2 -> 1 for Python's 0 indexing; doco update

--- neutral_surfaces/mixed_layer.py
+++ neutral_surfaces/mixed_layer.py
@@ -3,49 +3,64 @@
 from neutral_surfaces.interp_ppc import linear_coeffs, val
 from neutral_surfaces.eos.eostools import vectorize_eos
 
-def mixed_layer(S, T, P, eos, pot_dens_diff=0.03, ref_p=100.0,bottle_num=2,interp_fn=linear_coeffs):
+def mixed_layer(S, T, P, eos, pot_dens_diff=0.03, ref_p=100.0,bottle_num=1,interp_fn=linear_coeffs):
     """Calculate the pressure or depth at the bottom of the mixed layer
-    
-    
-    ML = mixed_layer(S, T, P)
-    calculates the pressure or depth of the mixed layer, ML, in an ocean
-    with practical / absolute salinity S and potential / Conservative
-    temperature T located at datasites where the pressure or depth is P.  The
-    equation of state for either the in-situ density or the specific volume
-    is given by eos.m in the path, which accepts S, T, P as its 3 inputs.
-    ML is the pressure or depth at which the potential density (referenced
-    to P = 100 dbar or P = 100 m) exceeds the potential density near the
-    surface (the second bottle on each cast) by 0.03 kg m^-3.
-    
-    ... = mixed_layer(..., OPTS) overwrites the default parameters
-    according to the struct OPTS (see below).
-    
+
+    The mixed layer is the pressure or depth at which the potential density 
+    (referenced to `ref_p`) exceeds the potential density near the surface 
+    (specifically, the n'th bottle on each cast where n = `bottle_num`) by an 
+    amount of `pot_dens_diff`.
     
     Parameters
     ----------
-    S [nk, ni, nj]: practical / Absolute salinity
-    T [nk, ni, nj]: potential / Conservative temperature
-    P [nk, ni, nj]: pressure [dbar] or depth [m, positive]
-    OPTS [struct]: options
-    OPTS.pot_dens_ref [1, 1]: the reference pressure or depth for potential
-    density [dbar or m, positive]
-    OPTS.pot_dens_diff [1, 1]: the potential density difference that
-    determines the mixed layer [kg m^-3]
-    OPTS.bottle_num [1, 1]: the bottle number on each cast where the "near
-    surface" potential density is calculated [integer]
-    OPTS.INTERPFN [function handle]: the vertical interpolation function
+    S, T : ndarray or xarray.DataArray
+
+        3D practical / Absolute salinity and potential / Conservative
+        temperature --- that is, a 2D array of 1D water columns or "casts"
+
+    P : ndarray or xarray.DataArray
+
+        In the non-Boussinesq case, `P` is the 3D pressure, sharing the same
+        dimensions as `S` and `T`.
+
+        In the Boussinesq case, `P` is the depth and can be 3D with the same
+        structure as `S` and `T`, or can be 1D with as many elements as there
+        are in the vertical dimension of `S` and `T`.
+        
+        Must increase monotonically along the first dimension (i.e. downwards).
+
+    eos : function
+
+        Equation of state for the density or specific volume as a function of
+        `S`, `T`, and `P` inputs.  
+        
+        This function should be @numba.njit decorated and need not be
+        vectorized, as it will be called many times with scalar inputs.
+
+    pot_dens_diff : float, Default 0.03
     
-    Note: nk is the maximum number of data points per water column,
-       ni is the number of data points in longitude,
-       nj is the number of data points in latitude.
+        Difference in potential density [kg m-3] between the near-surface and 
+        the base of the mixed layer.
+        
+    ref_p : float, Default 100.0
     
-    Note: P must increase monotonically along the first dimension.
+        Reference pressure for potential density [dbar or m].
+        
+    bottle_num : int, Default 2
     
+        The bottle number on each cast where the "near surface" potential 
+        density is calculated.
+        
+    interp_fn : function, Default `linear_coeffs`
     
+        Function to perform vertical interpolation
+
+        
     Returns
     -------
     ML : ndarray
-        the pressure [dbar] or depth [m, positive] of the mixed layer
+        
+        A 2D array giving the pressure [dbar] or depth [m, positive] of the mixed layer
     """
     
     # Ensure eos is vectorized. It's okay if eos already was.
