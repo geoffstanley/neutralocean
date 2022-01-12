d4ef9a00880e780bc721fcd2d8f25476f07d1c6e
Geoff Stanley <geoffstanley@gmail.com>
2022-01-12 15:15:39 +1100

    use wget to download OCCA data

--- examples/run_OCCA.py
+++ examples/run_OCCA.py
@@ -7,16 +7,104 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from neutral_surfaces import sigma_surf, delta_surf, omega_surf
-from neutral_surfaces.data import load_OCCA
-from neutral_surfaces.ntp import ntp_ϵ_errors_norms, veronis_density
+from neutral_surfaces.ntp import ntp_ϵ_errors_norms
+from neutral_surfaces.label import veronis_density
 from neutral_surfaces.mixed_layer import mixed_layer
 from neutral_surfaces.eos.eostools import make_eos, make_eos_s_t, make_eos_p
 from neutral_surfaces.lib import _process_casts
 from neutral_surfaces.interp_ppc import linear_coeffs, val2
 
+# %% Prepare for loading OCCA data
+import xarray as xr
+import wget
+
+# ** ADJUST AS NEEDED **
+path_nspy = "/home/stanley/work/projects-gfd/neutral-surfaces-python/"
+# path_occa = "~/work/data/OCCA/"  # ** ADJUST AS NEEDED **
+path_occa = path_nspy + "examples/"
+
+# URL = 'ftp://mit.ecco-group.org/ecco_for_las/OCCA_1x1_v2/2004-6/annual/'
+url = "https://www.dropbox.com/s/q9hywvjup1mwhc9/DDsalt.0406annclim.nc"
+wget.download(url, path_occa)
+url = "https://www.dropbox.com/s/qr6bivfyk0s06ot/DDtheta.0406annclim.nc"
+wget.download(url, path_occa)
+
+
+def load_OCCA(OCCA_dir, ts=0):
+
+    # Read grid info from the theta nc file
+    x = xr.open_dataset("%sDD%s.0406annclim.nc" % (OCCA_dir, "theta")).load()
+
+    # Build our own grid, as the MITgcm does it
+    Pa2db = 1e-4
+    deg2rad = np.pi / 180
+
+    g = dict()  # model grid and parameters
+    g["ρ_c"] = 1027.5  # A guess. Same as ECCO2
+    g["grav"] = 9.81  # A guess. Same as ECCO2
+    g["rSphere"] = 6.37e6  # A guess. Same as ECCO2
+    g["resx"] = 1  # 1 grid cell per zonal degree
+    g["resy"] = 1  # 1 grid cell per meridional degree
+    g["wrap"] = (True, False)  # periodic in longitude, not in latitude
+
+    # Lateral coordinates
+    g["XCvec"] = np.require(x.Longitude_t.values, dtype=np.float64, requirements="C")
+    g["XGvec"] = np.require(x.Longitude_u.values, dtype=np.float64, requirements="C")
+    g["YCvec"] = np.require(x.Latitude_t.values, dtype=np.float64, requirements="C")
+    g["YGvec"] = np.require(x.Latitude_v.values, dtype=np.float64, requirements="C")
+
+    # Lateral distances
+    g["DXGvec"] = g["rSphere"] * np.cos(g["YGvec"] * deg2rad) / g["resx"] * deg2rad
+    g["DYGsc"] = g["rSphere"] * deg2rad / g["resy"]
+    g["DXCvec"] = g["rSphere"] * np.cos(g["YCvec"] * deg2rad) / g["resx"] * deg2rad
+    g["DYCsc"] = g["DYGsc"]
+
+    # Vertical coordinate and distances
+    g["RC"] = -np.require(x.Depth_c, dtype=np.float64, requirements="C")
+    g["DRC"] = np.diff(-g["RC"])
+
+    g["nx"] = g["XCvec"].size
+    g["ny"] = g["YCvec"].size
+    g["nz"] = g["RC"].size
+
+    # Vertical area of the tracer cells [m^2]
+    g["RACvec"] = (g["rSphere"] ** 2 / g["resx"] * deg2rad) * abs(
+        np.sin((g["YGvec"] + 1 / g["resy"]) * deg2rad) - np.sin(g["YGvec"] * deg2rad)
+    )
+
+    T = x.theta.isel(Time=ts)
+    x.close()
+
+    x = xr.open_dataset("%sDD%s.0406annclim.nc" % (OCCA_dir, "salt")).load()
+    S = x.salt.isel(Time=ts)
+    x.close()
+
+    # phihyd = Pres / rho_c +  grav * z
+    x = xr.open_dataset("%sDD%s.0406annclim.nc" % (OCCA_dir, "phihyd")).load()
+    P = x.phihyd.isel(Time=ts)
+
+    # convert to full in-situ pressure, in [dbar]
+    Z3D = -g["RC"].reshape(tuple(-1 if x == "Depth_c" else 1 for x in P.dims))
+    P = (P + g["grav"] * Z3D) * (g["ρ_c"] * Pa2db)
+    x.close()
+
+    x = xr.open_dataset("%sDD%s.0406annclim.nc" % (OCCA_dir, "etan"))
+    η = x.etan.isel(Time=ts)
+    x.close()
+
+    # Reorder dimensions to ensure individual water columns are float64 and contiguous in memory
+    dims = ("Longitude_t", "Latitude_t", "Depth_c")
+    S, T, P = (x.transpose(*dims).astype(np.float64, order="C") for x in (S, T, P))
+    η = η.transpose(*dims[0:-1]).astype(np.float64, order="C")
+
+    # ATMP = 0.  # Atmospheric Pressure (loading)
+    # SAP = 0.  # Standard Atmospheric Pressure
+
+    return g, S, T, P, η
+
+
 # %% Load OCCA data
-path_occa = "~/work/data/OCCA/"  # ** ADJUST AS NEEDED **
-g, S, T, _, _ = load_OCCA(path_occa)  # S and T are arranged as (Longitude, Latitude, Depth)
+g, S, T, _, _ = load_OCCA(path_occa)  # S arranged as (Longitude, Latitude, Depth)
 ni, nj, nk = S.shape
 Z = -g["RC"]  # Depth vector (note positive and increasing down)
 
@@ -45,9 +133,7 @@ s, t, z, d = sigma_surf(
     S,
     T,
     Z,
-    eos="jmd95",
-    grav=g["grav"],
-    rho_c=g["ρ_c"],
+    eos=eos,
     wrap="Longitude_t",
     vert_dim="Depth_c",
     ref=0.0,
@@ -79,7 +165,7 @@ s, t, z, d = sigma_surf(
     eos_s_t=eos_s_t,
     wrap="Longitude_t",
     vert_dim="Depth_c",
-    pin_cast={"Longitude_t" : 180.5, "Latitude_t" : 0.5},
+    pin_cast={"Longitude_t": 180.5, "Latitude_t": 0.5},
     pin_p=z0,
 )
 
@@ -104,9 +190,7 @@ s, t, z, d = delta_surf(
     S,
     T,
     Z,
-    eos="jmd95",
-    grav=g["grav"],
-    rho_c=g["ρ_c"],
+    eos=eos,
     wrap="Longitude_t",
     vert_dim="Depth_c",
     ref=(s0, t0),
@@ -153,9 +237,7 @@ s, t, z, d = omega_surf(
     vert_dim="Depth_c",
     pin_cast=(i0, j0),
     pin_p=z0,
-    eos="jmd95",
-    grav=g["grav"],
-    rho_c=g["ρ_c"],
+    eos=eos,
     ITER_MAX=10,
     ITER_START_WETTING=1,
 )
@@ -166,7 +248,7 @@ print(f'   update time: {np.sum(d["timer_update"]) : .4f} sec')
 
 # Initialize omega surface with a (locally referenced) delta surface.
 # Also remove the pre-computed mixed layer.  Could also pass
-# p_ml={"bottle_index" : 1, "ref_p" : 0.0} 
+# p_ml={"bottle_index" : 1, "ref_p" : 0.0}
 # for example, to compute mixed layer internally with the given parameters.
 s, t, z, d = omega_surf(
     S,
@@ -177,9 +259,7 @@ s, t, z, d = omega_surf(
     vert_dim="Depth_c",
     pin_cast=(i0, j0),
     pin_p=z0,
-    eos="jmd95",
-    grav=g["grav"],
-    rho_c=g["ρ_c"],
+    eos=eos,
     ITER_MAX=10,
     ITER_START_WETTING=1,
     TOL_P_SOLVER=1e-5,
@@ -238,9 +318,7 @@ from neutral_surfaces.lib import find_first_nan
 n_good = find_first_nan(S1)[()]
 S1ppc = linear_coeffs(Z, S1)
 T1ppc = linear_coeffs(Z, T1)
-s1, t1, z1 = _ntp_bottle_to_cast(
-    sB, tB, zB, S1, T1, Z, S1ppc, T1ppc, n_good, eos, 1e-4
-)
+s1, t1, z1 = _ntp_bottle_to_cast(sB, tB, zB, S1, T1, Z, S1ppc, T1ppc, n_good, eos, 1e-4)
 
 
 # %% Veronis Density, used to label an approx neutral surface
@@ -251,14 +329,13 @@ T_ref_cast = T.values[i0, j0]
 )  # 1027.7700462375435
 
 
-
 # %% Work with Numpy arrays instead of xarrays
 
 # Convert S and T from xarray to numpy ndarrays, and make vertical dimension contiguous in memory.
-# If not done here in advance, this will be done each time sigma_surf, delta_surf, or omega_surf is called. 
+# If not done here in advance, this will be done each time sigma_surf, delta_surf, or omega_surf is called.
 S, T, Z = _process_casts(S, T, Z, "Depth_c")
 
-# %% 
+# %%
 s, t, z, d = delta_surf(
     S,
     T,
@@ -274,15 +351,23 @@ s, t, z, d = delta_surf(
 
 # %% Calculate a large-scale potential vorticity on our surface
 
-Earth_day = 86164 # Earth sidereal day period [s]
-f = 2 * (2 * np.pi / Earth_day) * np.sin(g["YCvec"] * (np.pi / 180))  # Coriolis param [s-1] on tracer grid
+# Earth sidereal day period [s]
+Earth_day = 86164
+
+# Coriolis param [s-1] on tracer grid
+f = 2 * (2 * np.pi / Earth_day) * np.sin(g["YCvec"] * (np.pi / 180))
 
 sz, tz = val2(Z, S, Sppc, T, Tppc, z, 1)  # ∂S/∂Z and ∂T/∂Z, on the surface
 rs, rt = eos_s_t(s, t, z)  # ∂ρ/∂S and ∂ρ/∂T, on the surface
 
-σz = rs * sz + rt * tz  # ∂σ/∂z on the surface, where σ is the locally reference potential density.
-q = f * σz  # large-scale potential vorticity [m-1 s-1] defined via locally referenced potential density, on the surface
+# ∂σ/∂z on the surface, where σ is the locally reference potential density.
+# σz = rs * sz + rt * tz
+
+# large-scale potential vorticity [m-1 s-1] defined via locally referenced potential density, on the surface
+# q = f * σz
 
 # ∂δ/∂z on the surface, where δ is the in-situ density anomaly
 δz = rs * sz + rt * tz + (eos_z(s, t, z) - eos_z(s0, t0, z))
-q = f * δz  # large-scale potential vorticity [m-1 s-1] defined via in-situ density anomaly, on the surface
+
+# large-scale potential vorticity [m-1 s-1] defined via in-situ density anomaly, on the surface
+q = f * δz
