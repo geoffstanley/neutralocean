3d667bb99e84a4293b51998005ad739a471979b9
Geoff Stanley <geoffstanley@gmail.com>
2022-01-17 15:01:36 +1100

    grid distances for ntp_ϵ_errors; doco update

--- neutralocean/ntp.py
+++ neutralocean/ntp.py
@@ -5,26 +5,57 @@ import numpy as np
 from neutralocean.lib import _process_wrap, xr_to_np
 
 
-def _ntp_ϵ_error1(s, t, p, eos_s_t, wrap, shift):
-    # Calculate neutrality error on a surface in one direction.
+def ntp_ϵ_errors(s, t, p, eos_s_t, wrap, dist1_iJ=1.0, dist2_Ij=1.0):
+    """
+    Calculate ϵ neutrality errors on an approximately neutral surface
 
-    sa, ta, pa = (avg(x, shift, wrap) for x in (s, t, p))
+    Parameters
+    ----------
+    s, t, p : ndarray
+        practical / Absolute Salinity, potential / Conservative temperature,
+        and pressure / depth on the surface
 
-    ds, dt = (dif(x, shift, wrap) for x in (s, t))
+    eos_s_t : function
+        Equation of state for the partial derivatives of density or specific
+        volume with respect to `S` and `T` as a function of `S`, `T`, and `P`
+        inputs.
 
-    rsa, rta = eos_s_t(sa, ta, pa)
-    return rsa * ds + rta * dt
+    wrap : tuple of bool, or tuple of str
 
+        Specifies which dimensions are periodic.
 
-def ntp_ϵ_errors(s, t, p, eos_s_t, wrap):
+        As a tuple of bool, this must be length two.  The first and second
+        dimensions of `s` and `t` are periodic iff ``wrap[0]`` and
+        ``wrap[1]`` is True, respectively.
+
+        As a tuple of str, simply name the periodic dimensions of `S` and
+        `T`.
+
+    dist1_iJ, dist2_Ij, : float or ndarray, Default 1.0
+
+        Grid distances [m] in either the 1st or 2nd lateral dimension, and
+        centred at the location specified.  The naming uses a soft notation:
+        the central grid point is(I,J), and i = I-1/2 and j = J-1/2.  Thus,
+        `dist1_iJ[5,3]` is the distance between cells (5,3) and (4,3), while
+        `dist2_iJ[5,3]` is the distance of the face between cells (5,3) and
+        (4,3). Similarly, `dist2_Ij[5,3]` is the distance between cells
+        (5,3) and (5,2), while `dist1_Ij[5,3]` is the distance of the face
+        between cells (5,3) and (5,2).
+
+    Returns
+    -------
+    ϵx, ϵy : ndarray
+        The ϵ neutrality errors (in the first and second lateral directions)
+        on the surface.
+    """
     # Calculate neutrality error on a surface.
     # Use backward differences; results are on the U, V grids.
 
     wrap = _process_wrap(wrap, s)
     s, t, p = (xr_to_np(x) for x in (s, t, p))
 
-    ϵx = _ntp_ϵ_error1(s, t, p, eos_s_t, wrap, im1)
-    ϵy = _ntp_ϵ_error1(s, t, p, eos_s_t, wrap, jm1)
+    ϵx = _ntp_ϵ_error1(s, t, p, eos_s_t, wrap, im1) / dist1_iJ
+    ϵy = _ntp_ϵ_error1(s, t, p, eos_s_t, wrap, jm1) / dist2_Ij
 
     return ϵx, ϵy
 
@@ -32,7 +63,37 @@ def ntp_ϵ_errors(s, t, p, eos_s_t, wrap):
 def ntp_ϵ_errors_norms(
     s, t, p, eos_s_t, wrap, dist1_iJ=1.0, dist1_Ij=1.0, dist2_Ij=1.0, dist2_iJ=1.0
 ):
+    """
+    Calculate norms of the ϵ neutrality errors on an approximately neutral surface
 
+    Parameters
+    ----------
+    s, t, p, eos_s_t, wrap :
+        See ntp_ϵ_errors
+
+    dist1_iJ, dist1_Ij, dist2_Ij, dist2_iJ : float or ndarray, Default 1.0
+
+        Grid distances [m] in either the 1st or 2nd lateral dimension, and
+        centred at the location specified.  The naming uses a soft notation:
+        the central grid point is(I,J), and i = I-1/2 and j = J-1/2.  Thus,
+        `dist1_iJ[5,3]` is the distance between cells (5,3) and (4,3), while
+        `dist2_iJ[5,3]` is the distance of the face between cells (5,3) and
+        (4,3). Similarly, `dist2_Ij[5,3]` is the distance between cells
+        (5,3) and (5,2), while `dist1_Ij[5,3]` is the distance of the face
+        between cells (5,3) and (5,2).
+
+    Returns
+    -------
+    ϵ_RMS : float
+        Area-weighted root-mean-square of the ϵ neutrality error on the surface
+
+    ϵ_MAV : TYPE
+        Area-weighted mean-absolute-value of the ϵ neutrality error on the surface
+
+    """
+
+    # Calculate ϵ neutrality errors.  Here, treat all distances = 1.
+    # The actual distances will be handled in computing the norms
     ϵ_iJ, ϵ_Ij = ntp_ϵ_errors(s, t, p, eos_s_t, wrap)
 
     ni, nj = s.shape
@@ -71,6 +132,17 @@ def ntp_ϵ_errors_norms(
     # fmt: on
 
     return ϵ_RMS, ϵ_MAV
+
+
+def _ntp_ϵ_error1(s, t, p, eos_s_t, wrap, shift):
+    # Calculate neutrality error on a surface in one direction.
+
+    sa, ta, pa = (avg(x, shift, wrap) for x in (s, t, p))
+
+    ds, dt = (dif(x, shift, wrap) for x in (s, t))
+
+    rsa, rta = eos_s_t(sa, ta, pa)
+    return rsa * ds + rta * dt
 
 
 def im1(F, wrap, fill=np.nan):  # G[i,j] == F[i-1,j]
