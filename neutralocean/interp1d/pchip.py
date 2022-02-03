1ffc24085a1cad1d119043b5b5bccd774c3b6938
Geoff Stanley <geoffstanley@gmail.com>
2022-02-03 13:47:23 +1100

    cleanup of commented out code

--- neutralocean/interp1d/pchip.py
+++ neutralocean/interp1d/pchip.py
@@ -2,46 +2,6 @@
 import numpy as np
 import numba as nb
 
-# from .interp import interp1d
-
-
-# def pchip_1d(x, X, Y, d=0, i=None):
-#     """
-#     Interpolation in one dimension
-
-#     Parameters
-#     ----------
-#     x : float
-#         The evaluation site
-
-#     X : ndarray(float, 1d)
-#         The independent data.
-
-#     Y : ndarray(float, 1d)
-#         The dependent data.
-
-#     d : int, Default 0
-#         Evaluate the `d`'th derivative of the interpolant.
-#         If 0, this simply evaluates the interpolant.
-
-#     i : int, Default None
-#         The interval of `X` that contains `x`.  Specifically,
-#             (a) `i == 1`  and  `X[0] <= x <= X[1]`, or
-#             (b) `2 <= i <= len(X) - 1`  and  X[i-1] < x <= X[i]`.
-#         These facts about `i` are assumed true; they are not checked.
-#         (This function will not be called if `x < X[0]` or `X[-1] < x` or
-#         `x` is nan or `X` are all nan.)
-#         If None, the correct value will be determined internally.
-
-#     Returns
-#     -------
-#     y : float
-#         The value (if `d==0`) or the derivative (if `d==1`) of the interpolant
-#         for `Y` in terms of `X` evaluated at `x`.
-#     """
-
-#     return interp1d(x, X, Y, (pchip_i, pchip_dx_i, pchip_dxx_i, pchip_dxxx_i), d, i)
-
 
 @nb.njit
 def _pchip(x, X, Y, i):
