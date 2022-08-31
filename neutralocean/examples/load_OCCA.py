3fc791c591a8670bf0802e148fdaf05ac37f8590
Geoff Stanley <geoffstanley@gmail.com>
2022-08-31 14:54:17 +1000

    cleanup for black

--- neutralocean/examples/load_OCCA.py
+++ neutralocean/examples/load_OCCA.py
@@ -25,8 +25,12 @@ def load_OCCA():
     url_salt = "ftp://mit.ecco-group.org/ecco_for_las/OCCA_1x1_v2/2004-6/annual/DDsalt.0406annclim.nc"
     url_theta = "ftp://mit.ecco-group.org/ecco_for_las/OCCA_1x1_v2/2004-6/annual/DDtheta.0406annclim.nc"
 
-    hash_salt = "4d90ab2c6cf524bb56afed54a66e33172a9ed57382ab11014a46618be3e199e5"
-    hash_theta = "f9f30bda7fa006be802e4f705fc7608943945ffba4b628c8ae33310d2f696ba4"
+    hash_salt = (
+        "4d90ab2c6cf524bb56afed54a66e33172a9ed57382ab11014a46618be3e199e5"
+    )
+    hash_theta = (
+        "f9f30bda7fa006be802e4f705fc7608943945ffba4b628c8ae33310d2f696ba4"
+    )
 
     file_salt = pooch.retrieve(url=url_salt, known_hash=hash_salt)
     file_theta = pooch.retrieve(url=url_theta, known_hash=hash_theta)
@@ -34,11 +38,11 @@ def load_OCCA():
     ts = 0  # Select the first time step (the only time step in these files)
 
     # Read grid info from the theta nc file
-    x = xr.open_dataset(file_theta).load()
+    ds = xr.open_dataset(file_theta).load()
 
     # Build our own grid, as the MITgcm does it
     # Pa2db = 1e-4
-    deg2rad = np.pi / 180
+    d2r = np.pi / 180
 
     g = dict()  # model grid and parameters
     g["ρ_c"] = 1027.5  # A guess. Same as ECCO2
@@ -49,19 +53,19 @@ def load_OCCA():
     g["wrap"] = (True, False)  # periodic in longitude, not in latitude
 
     # Lateral coordinates
-    g["XCvec"] = np.require(x.Longitude_t.values, dtype=np.float64, requirements="C")
-    g["XGvec"] = np.require(x.Longitude_u.values, dtype=np.float64, requirements="C")
-    g["YCvec"] = np.require(x.Latitude_t.values, dtype=np.float64, requirements="C")
-    g["YGvec"] = np.require(x.Latitude_v.values, dtype=np.float64, requirements="C")
+    g["XCvec"], g["XGvec"], g["YCvec"], g["YGvec"] = (
+        np.require(ds[x].values, dtype=np.float64, requirements="C")
+        for x in ("Longitude_t", "Longitude_u", "Latitude_t", "Latitude_v")
+    )
 
     # Lateral distances
-    g["DXGvec"] = g["rSphere"] * np.cos(g["YGvec"] * deg2rad) / g["resx"] * deg2rad
-    g["DYGsc"] = g["rSphere"] * deg2rad / g["resy"]
-    g["DXCvec"] = g["rSphere"] * np.cos(g["YCvec"] * deg2rad) / g["resx"] * deg2rad
+    g["DXGvec"] = g["rSphere"] * np.cos(g["YGvec"] * d2r) / g["resx"] * d2r
+    g["DYGsc"] = g["rSphere"] * d2r / g["resy"]
+    g["DXCvec"] = g["rSphere"] * np.cos(g["YCvec"] * d2r) / g["resx"] * d2r
     g["DYCsc"] = g["DYGsc"]
 
     # Vertical coordinate and distances
-    g["RC"] = -np.require(x.Depth_c, dtype=np.float64, requirements="C")
+    g["RC"] = -np.require(ds.Depth_c, dtype=np.float64, requirements="C")
     g["DRC"] = np.diff(-g["RC"])
 
     g["nx"] = g["XCvec"].size
@@ -69,16 +73,16 @@ def load_OCCA():
     g["nz"] = g["RC"].size
 
     # Vertical area of the tracer cells [m^2]
-    g["RACvec"] = (g["rSphere"] ** 2 / g["resx"] * deg2rad) * abs(
-        np.sin((g["YGvec"] + 1 / g["resy"]) * deg2rad) - np.sin(g["YGvec"] * deg2rad)
+    g["RACvec"] = (g["rSphere"] ** 2 / g["resx"] * d2r) * abs(
+        np.sin((g["YGvec"] + 1 / g["resy"]) * d2r) - np.sin(g["YGvec"] * d2r)
     )
 
-    T = x.theta.isel(Time=ts)
-    x.close()
+    T = ds.theta.isel(Time=ts)
+    ds.close()
 
-    x = xr.open_dataset(file_salt).load()
-    S = x.salt.isel(Time=ts)
-    x.close()
+    ds = xr.open_dataset(file_salt).load()
+    S = ds.salt.isel(Time=ts)
+    ds.close()
 
     # Reorder dimensions to ensure individual water columns are float64 and contiguous in memory
     dims = ("Longitude_t", "Latitude_t", "Depth_c")
