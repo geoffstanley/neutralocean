48954bf2a4f53385157a8af2ebb212b08ca8bd1c
Geoff Stanley <geoffstanley@gmail.com>
2023-05-15 15:15:50 -0700

    Fix SSS partial derivative of GSW specvol; add new public functions
    - It was missing one term, though so tiny it was not noticed.
    - Add public functions for all first, all second, all third order derivs
    - Update docs
    - Update checkvals
    - All partial derivatives have been checked using mpmath to approximate
      against finite difference approximations; all functions are correct
      (now).

--- neutralocean/eos/gsw.py
+++ neutralocean/eos/gsw.py
@@ -3,14 +3,27 @@ Density of Sea Water using TEOS-10 Gibbs Sea Water [1]_ in pure Python
 
 Functions:
 
-specvol :: computes specific volume from Absolute Salinity, Conservative Temperature and
+specvol :: compute specific volume from Absolute Salinity, Conservative Temperature and
     pressure
 
-specvol_s_t :: compute the partial derivatives of specific volume th
-    respect to Absolute Salinity and Conservative Temperature
+specvol_first_derivs :: compute first order partial derivatives of specvol
 
-specvol_p :: compute the partial derivative of specific volume with
-    respect to pressure
+specvol_second_derivs :: compute second order partial derivatives of specvol
+
+specvol_third_derivs :: compute third order partial derivatives of specvol
+
+specvol_s_t :: compute the partial derivatives of specvol with respect to 
+    Absolute Salinity and Conservative Temperature
+
+specvol_p :: compute the partial derivative of specvol with respect to pressure
+
+specvol_s_t_ss_st_tt_sp_tp :: compute various partial derivatives of specvol,
+    namely with respect to s, t, ss, st, tt, sp, tp.
+
+specvol_s_t_ss_st_tt_sp_tp_sss_sst_stt_ttt_ssp_stp_ttp_spp_tpp :: compute various 
+    partial derivatives of specvol, namely with respect to 
+    s, t, ss, st, tt, sp, tp, sss, sst, stt, ttt, ssp, stp, ttp, spp, tpp.
+
 
 Notes:
 To make Boussinesq versions of these functions, see 
@@ -83,26 +96,33 @@ All functions here are derived from `gsw_specvol`, documented below.
        ISBN 978-0-646-55621-5. 
 """
 
-# Check value computed on 16/10/2022, and agrees with MATLAB function
-# gsw_specvol_third_derivatives_st.m in neutral-surfaces toolbox.
+# Check values computed on 15/05/2023:
 #
-# gsw.specvol_s_t_ss_st_tt_sp_tp_sss_sst_stt_ttt_ssp_stp_ttp_spp_tpp(35, 2, 100)
-# (-7.524044242941373e-07,
-#  7.835130137433233e-08,
-#  1.0840451733910375e-09,
-#  2.747547886004396e-09,
-#  1.186819176528437e-08,
-#  1.5020065837658017e-11,
-#  2.6031411152070758e-11,
-#  -1.3470792464764363e-12,
-#  -1.6001306157869825e-11,
-#  -6.839037913975825e-11,
-#  -3.491354850608456e-10,
-#  -4.925365428774398e-14,
-#  -1.8246324672046492e-13,
-#  -9.922059938783903e-13,
-#  -6.426641694819895e-16,
-#  -1.0860670553447786e-15)
+# >>> specvol(35, 25, 2000)
+# 0.000969429311180351
+# 
+# >>> specvol_first_derivs(35, 25, 2000)
+# (-6.842441315932168e-07, 3.095166333930331e-07, -3.821664447055098e-09)
+# 
+# >>> specvol_second_derivs(35, 25, 2000)
+# (7.521204828271216e-10,
+#  1.2378264339269954e-09,
+#  6.651570967433596e-09,
+#  1.0954594146512299e-11,
+#  9.280142767497533e-12,
+#  1.1313341817551131e-13)
+# 
+# >>> specvol_third_derivs(35, 25, 2000)
+# (-1.319511372984796e-12,
+#  -6.0928845552165204e-12,
+#  -3.089599059140399e-11,
+#  -8.493314427391567e-11,
+#  -4.639432556145182e-14,
+#  -9.086401239917513e-14,
+#  -4.3380635019322105e-13,
+#  -5.163347063050122e-16,
+#  -4.907754242711458e-16,
+#  -5.163491628438305e-18)
 
 
 import numpy as np
@@ -118,81 +138,81 @@ sfac = 0.0248826675584615
 offset = 5.971840214030754e-1
 
 # fmt: off
-v000 =  1.0769995862e-3 
-v001 = -6.0799143809e-5 
-v002 =  9.9856169219e-6 
-v003 = -1.1309361437e-6 
-v004 =  1.0531153080e-7 
-v005 = -1.2647261286e-8 
-v006 =  1.9613503930e-9 
-v010 = -1.5649734675e-5 
-v011 =  1.8505765429e-5 
-v012 = -1.1736386731e-6 
-v013 = -3.6527006553e-7 
-v014 =  3.1454099902e-7 
-v020 =  2.7762106484e-5 
-v021 = -1.1716606853e-5 
-v022 =  2.1305028740e-6 
-v023 =  2.8695905159e-7 
-v030 = -1.6521159259e-5 
-v031 =  7.9279656173e-6 
-v032 = -4.6132540037e-7 
-v040 =  6.9111322702e-6 
-v041 = -3.4102187482e-6 
-v042 = -6.3352916514e-8 
-v050 = -8.0539615540e-7 
-v051 =  5.0736766814e-7 
+v000 =  1.0769995862e-3
+v001 = -6.0799143809e-5
+v002 =  9.9856169219e-6
+v003 = -1.1309361437e-6
+v004 =  1.0531153080e-7
+v005 = -1.2647261286e-8
+v006 =  1.9613503930e-9
+v010 = -1.5649734675e-5
+v011 =  1.8505765429e-5
+v012 = -1.1736386731e-6
+v013 = -3.6527006553e-7
+v014 =  3.1454099902e-7
+v020 =  2.7762106484e-5
+v021 = -1.1716606853e-5
+v022 =  2.1305028740e-6
+v023 =  2.8695905159e-7
+v030 = -1.6521159259e-5
+v031 =  7.9279656173e-6
+v032 = -4.6132540037e-7
+v040 =  6.9111322702e-6
+v041 = -3.4102187482e-6
+v042 = -6.3352916514e-8
+v050 = -8.0539615540e-7
+v051 =  5.0736766814e-7
 v060 =  2.0543094268e-7
-v100 = -3.1038981976e-4 
-v101 =  2.4262468747e-5 
-v102 = -5.8484432984e-7 
-v103 =  3.6310188515e-7 
+v100 = -3.1038981976e-4
+v101 =  2.4262468747e-5
+v102 = -5.8484432984e-7
+v103 =  3.6310188515e-7
 v104 = -1.1147125423e-7
-v110 =  3.5009599764e-5 
-v111 = -9.5677088156e-6 
-v112 = -5.5699154557e-6 
-v113 = -2.7295696237e-7 
-v120 = -3.7435842344e-5 
-v121 = -2.3678308361e-7 
-v122 =  3.9137387080e-7 
-v130 =  2.4141479483e-5 
-v131 = -3.4558773655e-6 
-v132 =  7.7618888092e-9 
-v140 = -8.7595873154e-6 
-v141 =  1.2956717783e-6 
-v150 = -3.3052758900e-7 
-v200 =  6.6928067038e-4 
-v201 = -3.4792460974e-5 
-v202 = -4.8122251597e-6 
-v203 =  1.6746303780e-8 
-v210 = -4.3592678561e-5 
-v211 =  1.1100834765e-5 
-v212 =  5.4620748834e-6 
-v220 =  3.5907822760e-5 
-v221 =  2.9283346295e-6 
-v222 = -6.5731104067e-7 
-v230 = -1.4353633048e-5 
-v231 =  3.1655306078e-7 
-v240 =  4.3703680598e-6 
-v300 = -8.5047933937e-4 
-v301 =  3.7470777305e-5 
-v302 =  4.9263106998e-6 
-v310 =  3.4532461828e-5 
-v311 = -9.8447117844e-6 
-v312 = -1.3544185627e-6 
-v320 = -1.8698584187e-5 
-v321 = -4.8826139200e-7 
+v110 =  3.5009599764e-5
+v111 = -9.5677088156e-6
+v112 = -5.5699154557e-6
+v113 = -2.7295696237e-7
+v120 = -3.7435842344e-5
+v121 = -2.3678308361e-7
+v122 =  3.9137387080e-7
+v130 =  2.4141479483e-5
+v131 = -3.4558773655e-6
+v132 =  7.7618888092e-9
+v140 = -8.7595873154e-6
+v141 =  1.2956717783e-6
+v150 = -3.3052758900e-7
+v200 =  6.6928067038e-4
+v201 = -3.4792460974e-5
+v202 = -4.8122251597e-6
+v203 =  1.6746303780e-8
+v210 = -4.3592678561e-5
+v211 =  1.1100834765e-5
+v212 =  5.4620748834e-6
+v220 =  3.5907822760e-5
+v221 =  2.9283346295e-6
+v222 = -6.5731104067e-7
+v230 = -1.4353633048e-5
+v231 =  3.1655306078e-7
+v240 =  4.3703680598e-6
+v300 = -8.5047933937e-4
+v301 =  3.7470777305e-5
+v302 =  4.9263106998e-6
+v310 =  3.4532461828e-5
+v311 = -9.8447117844e-6
+v312 = -1.3544185627e-6
+v320 = -1.8698584187e-5
+v321 = -4.8826139200e-7
 v330 =  2.2863324556e-6
-v400 =  5.8086069943e-4 
-v401 = -1.7322218612e-5 
-v402 = -1.7811974727e-6 
-v410 = -1.1959409788e-5 
-v411 =  2.5909225260e-6 
-v420 =  3.8595339244e-6 
-v500 = -2.1092370507e-4 
-v501 =  3.0927427253e-6 
+v400 =  5.8086069943e-4
+v401 = -1.7322218612e-5
+v402 = -1.7811974727e-6
+v410 = -1.1959409788e-5
+v411 =  2.5909225260e-6
+v420 =  3.8595339244e-6
+v500 = -2.1092370507e-4
+v501 =  3.0927427253e-6
 v510 =  1.3864594581e-6
-v600 =  3.1932457305e-5 
+v600 =  3.1932457305e-5
 # fmt: on
 
 
@@ -221,6 +241,102 @@ def specvol(SA, CT, p):
     """
     (x, y, z, _) = _process(SA, CT, p)
     return _specvol(x, y, z)
+
+
+@nb.njit
+def specvol_first_derivs(SA, CT, p):
+    """
+    Calculate all first order partial derivatives of GSW specific volume
+
+    Parameters
+    ----------
+    SA : float
+        Absolute Salinity [g/kg]
+    CT : float
+        Conservative Temperature [deg C]
+    p : float
+        sea pressure (i.e. absolute pressure - 10.1325 dbar)  [dbar]
+
+    Returns
+    -------
+    ss, st, tt, sp, tp, pp : float
+        Partial derivatives of specific volume.
+    """
+
+    (x, y, z, _) = _process(SA, CT, p)
+    s = _s(x, y, z)
+    t = _t(x, y, z)
+    p = _p(x, y, z)
+
+    return (s, t, p)
+
+
+@nb.njit
+def specvol_second_derivs(SA, CT, p):
+    """
+    Calculate all second order partial derivatives of GSW specific volume
+
+    Parameters
+    ----------
+    SA : float
+        Absolute Salinity [g/kg]
+    CT : float
+        Conservative Temperature [deg C]
+    p : float
+        sea pressure (i.e. absolute pressure - 10.1325 dbar)  [dbar]
+
+    Returns
+    -------
+    ss, st, tt, sp, tp, pp : float
+        Partial derivatives of specific volume.
+    """
+
+    (x, y, z, x2) = _process(SA, CT, p)
+
+    ss = _ss(x, y, z, x2)
+    st = _st(x, y, z)
+    tt = _tt(x, y, z)
+    sp = _sp(x, y, z)
+    tp = _tp(x, y, z)
+    pp = _pp(x, y, z)
+
+    return (ss, st, tt, sp, tp, pp)
+
+
+@nb.njit
+def specvol_third_derivs(SA, CT, p):
+    """
+    Calculate all third order partial derivatives of GSW specific volume
+
+    Parameters
+    ----------
+    SA : float
+        Absolute Salinity [g/kg]
+    CT : float
+        Conservative Temperature [deg C]
+    p : float
+        sea pressure (i.e. absolute pressure - 10.1325 dbar)  [dbar]
+
+    Returns
+    -------
+    sss, sst, stt, ttt, ssp, stp, ttp, spp, tpp : float
+        Partial derivatives of specific volume.
+    """
+
+    (x, y, z, x2) = _process(SA, CT, p)
+
+    sss = _sss(x, y, z, x2)
+    sst = _sst(x, y, z, x2)
+    stt = _stt(x, y, z)
+    ttt = _ttt(x, y, z)
+    ssp = _ssp(x, y, z, x2)
+    stp = _stp(x, y, z)
+    ttp = _ttp(x, y, z)
+    spp = _spp(x, y, z)
+    tpp = _tpp(x, y, z)
+    ppp = _ppp(x, y, z)
+
+    return (sss, sst, stt, ttt, ssp, stp, ttp, spp, tpp, ppp)
 
 
 @nb.njit
@@ -582,7 +698,8 @@ def _sss(x, y, z, x2):
        + y*(0.375*v122
        + y*(0.375*v132)))
     + z*(   0.375*v103
-       + y*(0.375*v113))))) / (x * x2 * x2) * (sfac**3)
+       + y*(0.375*v113)
+    + z * ( 0.375*v104))))) / (x * x2 * x2) * (sfac**3)
 
 
 @nb.njit
