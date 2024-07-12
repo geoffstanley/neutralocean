6eb43faf40705020d64351063d73c0d7349464eb
Geoff Stanley <geoffstanley@gmail.com>
2024-07-12 16:16:23 -0700

    import whole eos modules; remove gswc tests

--- neutralocean/tests/test_eos.py
+++ neutralocean/tests/test_eos.py
@@ -2,37 +2,21 @@ import numpy as np
 import pytest
 
 from neutralocean.eos.tools import vectorize_eos
-
-from neutralocean.eos.jmd95 import rho as rho_jmd95
-from neutralocean.eos.jmd95 import rho_s_t as rho_s_t_jmd95
-from neutralocean.eos.jmd95 import rho_p as rho_p_jmd95
-
-from neutralocean.eos.jmdfwg06 import rho as rho_jmdfwg06
-from neutralocean.eos.jmdfwg06 import rho_s_t as rho_s_t_jmdfwg06
-from neutralocean.eos.jmdfwg06 import rho_p as rho_p_jmdfwg06
-
-from neutralocean.eos.gswc import rho as rho_gsw
-from neutralocean.eos.gswc import rho_s_t as rho_s_t_gsw
-from neutralocean.eos.gswc import rho_p as rho_p_gsw
-
-from neutralocean.eos.gsw import specvol as specvol_gsw
-from neutralocean.eos.gsw import specvol_s_t as specvol_s_t_gsw
-from neutralocean.eos.gsw import specvol_p as specvol_p_gsw
+from neutralocean.eos import jmd95, jmdfwg06, gsw
 
 checkval_jmd95 = (35.5, 3.0, 3000.0, 1041.83267)
-rho_jmd95_ufunc = vectorize_eos(rho_jmd95)
+rho_jmd95_ufunc = vectorize_eos(jmd95.rho)
 
 # Check Values from Jackett and McDougall (1995) Appendix, p. 388
 # and Jackett et al (2006) Appendix A, p. 1723
 @pytest.mark.parametrize(
     "eos,checkval,decimals",
     [
-        (rho_jmd95, checkval_jmd95, 5),
-        (rho_jmdfwg06, (35.0, 25.0, 2000.0, 1031.65056056576), 11),
-        (rho_jmdfwg06, (20.0, 20.0, 1000.0, 1017.72886801964), 11),
-        (rho_jmdfwg06, (40.0, 12.0, 8000.0, 1062.95279820631), 11),
-        (rho_gsw, (35.0, 25.0, 2000.0, 1031.534727150376), 12),
-        (specvol_gsw, (35.0, 25.0, 2000.0, 9.694293111803510e-04), 18),
+        (jmd95.rho, checkval_jmd95, 5),
+        (jmdfwg06.rho, (35.0, 25.0, 2000.0, 1031.65056056576), 11),
+        (jmdfwg06.rho, (20.0, 20.0, 1000.0, 1017.72886801964), 11),
+        (jmdfwg06.rho, (40.0, 12.0, 8000.0, 1062.95279820631), 11),
+        (gsw.specvol, (35.0, 25.0, 2000.0, 9.694293111803510e-04), 18),
     ],
 )
 def test_checkval(eos, checkval, decimals):
@@ -57,10 +41,9 @@ def test_jmd95_ufunc_array():
 @pytest.mark.parametrize(
     "eos,eos_s_t,eos_p",
     [
-        (rho_jmd95, rho_s_t_jmd95, rho_p_jmd95),
-        (rho_jmdfwg06, rho_s_t_jmdfwg06, rho_p_jmdfwg06),
-        (rho_gsw, rho_s_t_gsw, rho_p_gsw),
-        (specvol_gsw, specvol_s_t_gsw, specvol_p_gsw),
+        (jmd95.rho, jmd95.rho_s_t, jmd95.rho_p),
+        (jmdfwg06.rho, jmdfwg06.rho_s_t, jmdfwg06.rho_p),
+        (gsw.specvol, gsw.specvol_s_t, gsw.specvol_p),
     ],
 )
 def test_rho_derivs(eos, eos_s_t, eos_p):
